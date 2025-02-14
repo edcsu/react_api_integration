25b1275759b539eb7b92adf6751878b447986780
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-14 16:40:22 +0300

    feat: handle errors gracefully

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,22 +1,44 @@
 import { useEffect, useState } from 'react'
 
 import Places from './Places.jsx';
+import ErrorPage from './Error.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailable] = useState([])
   const [isFetching, setIsFetching] = useState(false)
+  const [error, setError] = useState()
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true)
-      const response = await fetch('http://localhost:3001/places')
-      const data = await response.json()
-      setAvailable(data.places)
+
+      try {  
+        const response = await fetch('http://localhost:3001/places')
+        const data = await response.json()
+
+        if (!response.ok) {
+          throw new Error("Faild to get places");
+        }
+
+        setAvailable(data.places)
+      } catch (error) {
+        setError({
+          message: error.message || 'Could not retrieve places, try again later'
+        })
+      }
+
       setIsFetching(false)
     }
 
     fetchPlaces()
   }, [])
+
+  if (error) {
+    <ErrorPage
+      title="An error occured"
+      message={error.message} 
+    />
+  }
 
   return (
     <Places
