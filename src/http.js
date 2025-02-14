52c50c8ee5bb3b2ab248639c68f5711926ca3e31
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-14 17:26:37 +0300

    feat: save available places

--- src/http.js
+++ src/http.js
@@ -8,3 +8,21 @@ export async function fetchAvailablePlaces(params) {
 
     return data.places
 }
+
+export async function updateUserPlaces(places) {
+    const response = await fetch('http://localhost:3001/user-places', {
+        method: 'PUT',
+        body: JSON.stringify({places}),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    const data = await response.json()
+
+    if (!response.ok) {
+        throw new Error("Faild to get places");
+    }
+
+    return data.message
+}
