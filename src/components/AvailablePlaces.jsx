acfbb21125d29aec24f407c48d42f312103ca9c3
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-14 14:08:15 +0300

    feat: fetch available places

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,6 +1,18 @@
+import { useState } from 'react'
+
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [available, setAvailable] = useState([])
+
+  fetch('http://localhost:3001/places')
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      setAvailable(data.places)
+    })
+
   return (
     <Places
       title="Available Places"
